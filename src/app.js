cd551645792bc2c35b2ed2e2065c9bcd44bb7038
dayanaferrer <dayanaferreira2@gmail.com>
2020-10-03 17:35:13 -0300

    adicionei index

--- src/app.js
+++ src/app.js
@@ -5,6 +5,10 @@ const musicas = require ('./routes/musicasRoutes');
 
 app.use('/', musicas);
 
+app.get('/', (req, res) => {
+    res.status(200).sendFile('./views/index.html', { root: __dirname })
+})
+
 app.get('*', (req, res) => {
     res.status(404).sendFile('./views/404.html', { root: __dirname })
 })
